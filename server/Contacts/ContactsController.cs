b9d423bd93360ab6d77e3c0fae57ae71610be977
Chikanma Chika-Onuegbu <chikanmaonuegbu@ymail.com>
2025-07-17 02:24:02 -0600

    added getall controller

--- server/Contacts/ContactsController.cs
+++ server/Contacts/ContactsController.cs
@@ -1,15 +1,42 @@
+using ContactsPage.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 public class ContactsController: BaseController
 {
-    // [HttpGet]
-    // public async Task<IActionResult> GetAllContacts([FromQuery] GetAllContactsRequest request)
-    // {
-    //     int page = (request?.Page > 0) ? request.Page : 1;
-    //     int pageSize = (request?.PageSize > 0) ? request.PageSize : 10;
+    private readonly ContactsDbContext _dbContext;
 
-        
+    public ContactsController(ContactsDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllContacts([FromQuery] GetAllContactsRequest request)
+    {
+        int page = ((request?.Page) > 0 ?  request?.Page: 1) ?? 0;
+        int contactsPerPage = ((request?.RecordsPerPage) > 0 ? request?.RecordsPerPage : 100) ?? 100;
 
 
+        IQueryable<Contact> query = _dbContext.Contacts
+        .Skip((page - 1) * contactsPerPage)
+        .Take(contactsPerPage);
 
+        if (request != null)
+        {
+            if (!string.IsNullOrWhiteSpace(request.FirstNameContains))
+            {
+                query = query.Where(e => e.FirstName.Contains(request.FirstNameContains));
+            }
+            
+            if (!string.IsNullOrWhiteSpace(request.LastNameContains))
+            {
+                query = query.Where(e => e.LastName.Contains(request.LastNameContains));
+            }
+        }
 
-    // }
+        var contacts = await query.ToArrayAsync();
+
+        return Ok(contacts);
+    }
 }
