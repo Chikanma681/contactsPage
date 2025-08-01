0fd51649bc71a8d4cf2235f100a6aacbe181be8f
Chikanma Chika-Onuegbu <chikanmaonuegbu@ymail.com>
2025-08-01 17:09:04 -0600

    started adding tests

--- server.Tests/UnitTest1.cs
+++ server.Tests/UnitTest1.cs
@@ -8,10 +8,55 @@ namespace server.Tests;
 
 public class BasicTests : IClassFixture<CustomWebApplicationFactory>
 {
+    private readonly CustomWebApplicationFactory _factory;
+    
+    public BasicTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+    }
     
     [Fact]
-    public void Test1()
+    public async Task GetAllContacts_ReturnsOkResult()
     {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/api/contacts");
 
+        if(!response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Request failed with status code {response.StatusCode}: {content}");
+        }
+
+        var contacts = await response.Content.ReadAsAsync<IEnumerable<GetEmployeeResponse>>();
+        Assert.NotNull(contacts);
     }
+
+    [Fact]
+    public async Task GetContactById_ReturnsOkResult()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/api/contacts/1");
+
+        if(!response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Request failed with status code {response.StatusCode}: {content}");
+        }
+
+        var contact = await response.Content.ReadAsAsync<GetEmployeeResponse>();
+        Assert.NotNull(contact);
+    }
+
+    [Fact]
+    public async Task CreateContact_ReturnsCreatedResult
+    {
+        var client = _factory.CreateClient();
+        var response = client.PostasJsonAsync("/api/contacts", new Contacts { FirstName = "John", LastName="Doe" });
+
+        response.EnsureSuccessStatusCode();
+    }
+
+
+    // [Fact]
+    // public async Task CreateE
 }
